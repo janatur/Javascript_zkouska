ed18d2549417f328687f79aa3372a5d03c10cf3f
janatur <137944471+janatur@users.noreply.github.com>
2023-08-02 15:09:02 +0200

    oprava radio

--- script.js
+++ script.js
@@ -4,37 +4,38 @@ const DisplayMessage = function (str) {
   document.querySelector(".message").textContent = str;
 };
 
-let pocetHorskychKol = Number(document.querySelector(".pocetHorske").value);
-let pocetDetskychKol = Number(document.querySelector(".pocetDetske").value);
-let pocetSilnicnichKol = Number(document.querySelector(".pocetSilnicni").value);
-let pocetGravelKol = Number(document.querySelector(".pocetGravel").value);
-
-console.log(pocetHorskychKol);
-console.log(pocetDetskychKol);
-console.log(pocetSilnicnichKol);
-console.log(pocetGravelKol);
-
-let pocetDnu = document.formular.pocetDnu.value;
-console.log(pocetDnu);
-
-let cyklonosic = document.querySelector('input[name="nosic"]:checked').value;
-
-console.log(cyklonosic);
-
-let cena =
-  (500 * pocetHorskychKol +
-    200 * pocetDetskychKol +
-    1500 * pocetSilnicnichKol +
-    2500 * pocetGravelKol) *
-  pocetDnu *
-  cyklonosic;
-console.log(cena);
-
-let rozpocet = Number(document.querySelector(".rozpocet").value);
-
-console.log(rozpocet);
-
 document.querySelector(".vypocet").addEventListener("click", function () {
+  let pocetHorskychKol = Number(document.querySelector(".pocetHorske").value);
+  let pocetDetskychKol = Number(document.querySelector(".pocetDetske").value);
+  let pocetSilnicnichKol = Number(
+    document.querySelector(".pocetSilnicni").value
+  );
+  let pocetGravelKol = Number(document.querySelector(".pocetGravel").value);
+
+  console.log(pocetHorskychKol);
+  console.log(pocetDetskychKol);
+  console.log(pocetSilnicnichKol);
+  console.log(pocetGravelKol);
+
+  let pocetDnu = document.formular.pocetDnu.value;
+  console.log(pocetDnu);
+
+  let cyklonosic = document.querySelector('input[name="nosic"]:checked').value;
+
+  console.log(cyklonosic);
+
+  let cena =
+    (500 * pocetHorskychKol +
+      200 * pocetDetskychKol +
+      1500 * pocetSilnicnichKol +
+      2500 * pocetGravelKol) *
+    pocetDnu *
+    cyklonosic;
+  console.log(cena);
+
+  let rozpocet = Number(document.querySelector(".rozpocet").value);
+
+  console.log(rozpocet);
   if (rozpocet >= cena) {
     DisplayMessage(
       `Tato zápůjčka vyhovuje Vašemu rozpočtu, bude stát ${cena},- Kč.`
@@ -46,7 +47,7 @@ document.querySelector(".vypocet").addEventListener("click", function () {
   }
 });
 
-function kontrolaDat() {
+/*function kontrolaDat() {
   let zadanaHodnota = document.querySelector(".email").value; // načtena hodnota (value) z políčka s name=email
   if (zadanaHodnota.indexOf("@") == -1) {
     // zavináč se nevyskytuje
@@ -54,4 +55,4 @@ function kontrolaDat() {
     return false;
   } else return true;
 }
-console.log(kontrolaDat(this));
+console.log(kontrolaDat(this));*/
