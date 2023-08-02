97d8a373614bb8598b3599bdc71979194ec6ea74
janatur <137944471+janatur@users.noreply.github.com>
2023-08-02 17:12:07 +0200

    opraveno

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
-"use strict";
-
-const DisplayMessage = function (str) {
-  document.querySelector(".message").textContent = str;
-};
-
-document.querySelector(".vypocet").addEventListener("click", function () {
+function vysledek() {
   let pocetHorskychKol = Number(document.querySelector(".pocetHorske").value);
   let pocetDetskychKol = Number(document.querySelector(".pocetDetske").value);
   let pocetSilnicnichKol = Number(
     document.querySelector(".pocetSilnicni").value
   );
   let pocetGravelKol = Number(document.querySelector(".pocetGravel").value);
-
   console.log(pocetHorskychKol);
   console.log(pocetDetskychKol);
   console.log(pocetSilnicnichKol);
   console.log(pocetGravelKol);
+  pocetDni = parseInt(document.form.pocetDnu.value);
 
-  let pocetDnu = document.formular.pocetDnu.value;
-  console.log(pocetDnu);
+  if (document.getElementById("radio1").checked) {
+    nosic = document.getElementById("radio1").value;
+  } else if (document.getElementById("radio2").checked) {
+    nosic = document.getElementById("radio2").value;
+  } else {
+    nosic = document.getElementById("radio3").value;
+  }
 
-  let cyklonosic = document.querySelector('input[name="nosic"]:checked').value;
-
-  console.log(cyklonosic);
-
-  let cena =
+  cena = Number(
     (500 * pocetHorskychKol +
       200 * pocetDetskychKol +
       1500 * pocetSilnicnichKol +
       2500 * pocetGravelKol) *
-    pocetDnu *
-    cyklonosic;
+      pocetDni *
+      nosic
+  );
   console.log(cena);
-
-  let rozpocet = Number(document.querySelector(".rozpocet").value);
-
-  console.log(rozpocet);
-  if (rozpocet >= cena) {
-    DisplayMessage(
-      `Tato zápůjčka vyhovuje Vašemu rozpočtu, bude stát ${cena},- Kč.`
-    );
+  let castka = Number(document.querySelector(".rozpocet").value);
+  console.log(castka);
+  if (castka >= cena) {
+    document.querySelector(
+      ".message"
+    ).textContent = `Tato zápůjčka vyhovuje Vašemu rozpočtu, bude stát ${cena},- Kč.`;
   } else {
-    DisplayMessage(
-      `Bohužel, tato zápůjčka je mimo Vaše možnosti. Stojí ${cena},- Kč.`
-    );
+    document.querySelector(
+      ".message"
+    ).textContent = `Bohužel, tato zápůjčka je mimo Vaše možnosti. Stojí ${cena},- Kč.`;
   }
-});
-
-/*function kontrolaDat() {
-  let zadanaHodnota = document.querySelector(".email").value; // načtena hodnota (value) z políčka s name=email
-  if (zadanaHodnota.indexOf("@") == -1) {
-    // zavináč se nevyskytuje
-    alert("neni zavináč");
-    return false;
-  } else return true;
 }
-console.log(kontrolaDat(this));*/
+
+function emailCheck() {
+  adresa = document.querySelector(".email").value;
+  if (adresa.indexOf("@") < 1) {
+    alert("Email má špatný formát");
+  } else alert("Objednávka byla odeslána");
+}
